54f78fb4e130c6caa111be98de4227529147f9a1
Elena Timofeeva <posazho@gmail.com>
2022-07-21 20:56:48 +0300

    числа от 1 до n с рекурсией

---
+++ Task63/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N, используя рекурсию.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+GetNumbers(N);
+
+void GetNumbers(int n, int i = 1)
+{
+    if (i > n) 
+    {
+        return;
+    }
+    if ( i == n)
+    {
+        Console.Write(i);
+    }
+    else
+    {
+        Console.Write($"{i}, ");
+    }
+    GetNumbers(n, i + 1);
+}
+
