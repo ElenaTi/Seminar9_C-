7d9c05fa519495d82cc4177a7142e4359536fd43
Elena Timofeeva <posazho@gmail.com>
2022-07-22 20:20:39 +0300

    сумма чисел от m до n

---
+++ Homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  */
+
+ int GetSumFromMToN(int m, int n)
+{
+    if (m > n) 
+    {
+        return 0;
+    }
+    else
+    {
+       return n + GetSumFromMToN(m, n - 1);
+    }
+    
+} 
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M > N)
+{
+    Console.WriteLine("Ошибка. M должно быть меньше N. Произведена замена M на N: ");
+    int temp = M;
+    M = N;
+    N = temp;
+}
+
+Console.WriteLine($"Сумма чисел от {M} до {N} равна: {GetSumFromMToN(M, N)}");
